43234d4ca22aaa20cb20af2da30e12aff972f837
Mohaimen Khan <mohaimenhasan@gmail.com>
2022-02-09 22:56:57 -0800

    Removed the mongo creds

--- consts/mongoCreds.js
+++
@@ -1,4 +0,0 @@
-exports.default = {
-    user: 'admin', // Don't change this for now. This is just so that we can use this in the env variables in the future
-    password: 'admin' // Don't change this for now. This is just so that we can use this in the env variables in the future
-}
--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const mongoCreds = require('./consts/mongoCreds');
 
 // TODO: Update this to an env
-// myFirstDataBase is for testing. Please uncomment the line after to put it to prod
-let dev_url =  `mongodb+srv://${mongoCreds.default.user}:${mongoCreds.default.password}@cluster0.e4fma.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-//let dev_url = `mongodb+srv://${mongoCreds.default.user}:${mongoCreds.default.password}@cluster0.e4fma.mongodb.net/rekkoInitial?retryWrites=true&w=majority`;
+let dev_url = undefined; // add local URL for testing here
 let mongoDB = process.env.MONGO_URI || dev_url;
+if (!mongoDB){
+    console.error('PLEASE ADD DB URI')
+}
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
